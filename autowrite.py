96c730967ef4c2aed9cf45e682354af93a24ab58
DKytgaming <101945037+DKytgaming@users.noreply.github.com>
2023-12-12 20:28:53 +0530

    Update autowrite.py

--- autowrite.py
+++ autowrite.py
@@ -4,7 +4,7 @@ import time as t
 msg=input("Enter the message you want to spam: ")
 no=int(input("Enter how many times you want spam the message: "))
 
-thi.sleep(3)
+t.sleep(3)
 
 for i in range(no):
     p.typewrite(msg)
